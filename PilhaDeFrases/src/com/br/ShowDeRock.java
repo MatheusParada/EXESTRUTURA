45957e430e32fbb99a6051f36cf8f36002b30ba7
MatheusParada <111541466+MatheusParada@users.noreply.github.com>
2023-09-15 21:31:23 -0300

    Update ShowDeRock.java

--- PilhaDeFrases/src/com/br/ShowDeRock.java
+++ PilhaDeFrases/src/com/br/ShowDeRock.java
@@ -1,25 +1,53 @@
 package com.br;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-import java.util.Stack;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
 
 public class ShowDeRock {
 
-    public static void main(String[] args) throws FileNotFoundException{
-        Scanner scanner;
-        Stack pilha = new Stack(); // Classe Pilha
+    public static void main(String[] args) {
+        // Crie uma fila para os ingressos vendidos
+        Queue<String> filaIngressos = new LinkedList<>();
 
-        scanner = new Scanner(new File("C:\\Users\\12114376\\IdeaProjects\\ExemplosEstruturaDados\\PilhaDeFrases\\Ingressos.txt"));
-        while (scanner.hasNextLine()){
-            pilha.push(scanner.nextLine());
+        // Leitura do arquivo de ingressos vendidos
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\12114376\\IdeaProjects\\ExemplosEstruturaDados\\PilhaDeFrases\\ingressos.txt"));
+            String linha;
+            while ((linha = br.readLine()) != null) {
+                filaIngressos.offer(linha); // Adiciona o ingresso à fila
+            }
+            br.close();
+        } catch (IOException e) {
+            System.err.println("Erro ao ler o arquivo de ingressos.");
+            return;
         }
-        System.out.println("Tamanho da Pilha " + pilha.size());
-        System.out.println("****Pilha de Nomes*****");
-        while(!pilha.empty())
-        {
-            System.out.println(pilha.pop());
+
+        // Exibe os 10 primeiros ingressos como entrada VIP
+        System.out.println("Entrada VIP:");
+        int contadorVIP = 0;
+        while (contadorVIP < 10 && !filaIngressos.isEmpty()) {
+            System.out.println(filaIngressos.poll());
+            contadorVIP++;
+        }
+
+        // Sorteia aleatoriamente uma posição da fila para a pessoa sortuda
+        Random random = new Random();
+        int tamanhoFila = filaIngressos.size();
+        int posicaoSorteada = random.nextInt(tamanhoFila);
+
+        // Exibe o ingresso da pessoa sortuda
+        System.out.println("\nPessoa sorteada para conhecer a banda:");
+        for (int i = 0; i < tamanhoFila; i++) {
+            String ingresso = filaIngressos.poll();
+            if (i == posicaoSorteada) {
+                System.out.println(ingresso + " (Pessoa sorteada para conhecer a banda)");
+            }
+            filaIngressos.offer(ingresso);
         }
     }
-
 }
